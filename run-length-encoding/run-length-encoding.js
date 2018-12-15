214971f6eaf18890b9d2b738ab86e768c8a0a6e7
Filip Kuszenin <filipkuszenin@gmail.com>
2018-12-15 18:59:34 +0000

    rna-transcription: initial commit

---
+++ run-length-encoding/run-length-encoding.js
@@ -0,0 +1,10 @@
+export
+
+function encode(notEncryptedString) {
+  let encryptedString = [];
+  const notEncryptedStringArray = notEncryptedString.split('');
+  return notEncryptedStringArray;
+
+  //create variable storing encrypted string
+  // convert String into array and store it as a variable
+}
