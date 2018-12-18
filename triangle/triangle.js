e29794751a9973b1f6281f80bb261c7a7f0f574b
Filip Kuszenin <filipkuszenin@gmail.com>
2018-12-18 19:54:03 +0000

    triangle: isosceles triangles have two first sides equal

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -7,16 +7,19 @@ const Triangle = function (side1, side2, side3) {
 
 Triangle.prototype.kind = function () {
 
-
-// create variable which stores all three sides
+const allSides = [this.side1, this.side2, this.side3];
 // create variable which stores value of first side;
 
 //loop over array of three sides
 
   if ( ( (this.side1 + this.side2) / 2) === (this.side3)) {
     return 'equilateral';
-  } else {
-    return 'test'
+  } else if (
+    (this.side1 === this.side2) || (this.side1 === this.side3)
+
+
+  ) {
+    return 'isosceles';
   }
 };
 
