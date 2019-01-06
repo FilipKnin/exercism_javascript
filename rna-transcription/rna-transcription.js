840aeca5c5e0973bfbcfc72ea01674a6261a979e
Filip Kuszenin <filipkuszenin@gmail.com>
2019-01-06 22:37:18 +0000

    rna-transcription: code refactored (map implemented)

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,47 +1,22 @@
 export
 
 function toRna(dna) {
-  const dnaArray = dna.split('');
-  let rna = [];
+  const dnaToRna = dna.split('');
+  // let rna = [];
 
-  dnaArray.map((dnaNucleotide) => {
+  let rna = dnaToRna.map((dnaNucleotide) => {
     if (dnaNucleotide === 'C') {
-        rna.push('G');
+        dnaNucleotide = 'G';
       } else if (dnaNucleotide === 'G') {
-        rna.push('C');
+        dnaNucleotide = 'C';
       } else if (dnaNucleotide === 'A') {
-        rna.push('U');
+        dnaNucleotide = 'U';
       } else if (dnaNucleotide === 'T') {
-        let rnaNucleotide = 'A';
-        rna.push('A');
+        dnaNucleotide = 'A';
       } else {
         throw 'Invalid input DNA.';
       }
+      return dnaNucleotide;
   })
   return rna.join('');
 }
-
-
-
-
-
-  // for (let i = 0; i < dnaArray.length; i++) {
-  //   let dnaNucleotide = dnaArray[i];
-  //   if (dnaNucleotide === 'C') {
-  //     let rnaNucleotide = 'G';
-  //     rna.push(rnaNucleotide);
-  //   } else if (dnaNucleotide === 'G') {
-  //     let rnaNucleotide = 'C';
-  //     rna.push(rnaNucleotide);
-  //   } else if (dnaNucleotide === 'A') {
-  //     let rnaNucleotide = 'U';
-  //     rna.push(rnaNucleotide);
-  //   } else if (dnaNucleotide === 'T') {
-  //     let rnaNucleotide = 'A';
-  //     rna.push(rnaNucleotide);
-  //   } else {
-  //     throw 'Invalid input DNA.';
-  //   }
-  //
-  // }
-  //   return rna.join('');
