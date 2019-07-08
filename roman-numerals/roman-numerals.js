3346c700f2e2dff4d920bf41e48b1f7f12ec4fda
Filip Kuszenin <filipkuszenin@gmail.com>
2019-07-08 11:28:14 +0200

    roman-numerals: some changes

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -3,6 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+// export const toRoman = (number) => {
+//   arabicNumeralsToRomanNumeralsConversion = {
+//     1: 'I',
+//     5: 'V',
+//     10: 'X',
+//     50: 'L',
+//     100: 'C',
+//     500: 'D',
+//     1000: 'M'
+//   }
+//
+// };
+
+
+export const toRoman = (number) => {
+  let roman = "";
+  const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
+  let a;
+  if(number < 1 || number > 3999)
+    return "Enter a number between 1 and 3999";
+  else{
+    for(let key in romanNumList){
+        a = Math.floor(number / romanNumList[key]);
+        if(a >= 0){
+            for(let i = 0; i < a; i++){
+              roman += key;
+            }
+          }
+        number = number % romanNumList[key];
+    }
+  }
+
+  return roman;
+}
